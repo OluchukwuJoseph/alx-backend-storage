c1399b0c3cb2470ba501f58e8f6256717945dba5
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-11-05 13:53:12 +0100

    Added files for task 6, 7 and 8

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+-- Creates a procedure AddBonus that adds a new correction for a student.
+CREATE PROCEDURE IF NOT EXISTS AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+  DECLARE project_id INT;
+  IF project_name NOT IN (SELECT name FROM projects) THEN
+    INSERT INTO projects (name) VALUES (project_name);
+    SET project_id = LAST_INSERT_ID();
+  ELSE
+    SET project_id = (SELECT id FROM projects WHERE name = project_name);
+  END IF;
+  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END$$
+
+DELIMITER ;
---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,10 @@
+-- Create stored procedure ComputeAverageScoreForUser
+DELIMITER $$
+CREATE PROCEDURE IF NOT EXISTS ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+  UPDATE users
+  SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+  WHERE id = user_id;
+END$$
+
+DELIMITER ;
