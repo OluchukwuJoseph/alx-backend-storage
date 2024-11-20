cc3f4aebc30f69625d8366c2e8953440d1286250
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-11-20 10:30:53 +0100

    Resolved issues

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     print(f"\tmethod POST: {collection.count_documents({'method': 'POST'})}")
     print(f"\tmethod PUT: {collection.count_documents({'method': 'PUT'})}")
     print(f"\tmethod PATCH: {collection.count_documents({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {collection.count_documents(
-        {'method': 'DELETE'})}")
+    print(f"\tmethod DELETE:"
+          f"{collection.count_documents({'method': 'DELETE'})}")
     print(f"{collection.count_documents({'method': 'GET', 'path': '/status'})}"
           " status check")
